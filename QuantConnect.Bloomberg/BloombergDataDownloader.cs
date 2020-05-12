1af2ac6ac8ac6c1285c2787a3af607fcb27d5561
Stefano Raggi <stefano.raggi67@gmail.com>
2020-05-12 16:09:37 +0200

    Add BloombergDataDownloader

---
+++ QuantConnect.Bloomberg/BloombergDataDownloader.cs
@@ -0,0 +1,61 @@
+﻿/*
+* QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+* Lean Algorithmic Trading Engine v2.2 Copyright 2015 QuantConnect Corporation.
+*/
+
+using System;
+using System.Collections.Generic;
+using QuantConnect.Data;
+using QuantConnect.ToolBox;
+
+namespace QuantConnect.Bloomberg
+{
+    public class BloombergDataDownloader : IDataDownloader
+    {
+        private readonly BloombergBrokerage _brokerage;
+
+        public BloombergDataDownloader(BloombergBrokerage brokerage)
+        {
+            _brokerage = brokerage;
+        }
+
+        /// <summary>
+        /// Get historical data enumerable for a single symbol, type and resolution given this start and end time (in UTC).
+        /// </summary>
+        /// <param name="symbol">Symbol for the data we're looking for.</param>
+        /// <param name="resolution">Resolution of the data request</param>
+        /// <param name="startUtc">Start time of the data in UTC</param>
+        /// <param name="endUtc">End time of the data in UTC</param>
+        /// <returns>Enumerable of base data for this symbol</returns>
+        public IEnumerable<BaseData> Get(Symbol symbol, Resolution resolution, DateTime startUtc, DateTime endUtc)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns an IEnumerable of Future/Option contract symbols for the given root ticker
+        /// </summary>
+        /// <param name="ticker">The root ticker</param>
+        /// <param name="securityType">Expected security type of the returned symbols (if any)</param>
+        /// <param name="includeExpired">Include expired contracts</param>
+        public IEnumerable<Symbol> GetChainSymbols(string ticker, SecurityType securityType, bool includeExpired)
+        {
+            return _brokerage.LookupSymbols(ticker, securityType, includeExpired);
+        }
+
+        /// <summary>
+        /// Downloads historical data from the brokerage and saves it in LEAN format.
+        /// </summary>
+        /// <param name="symbols">The list of symbols</param>
+        /// <param name="tickType">The tick type</param>
+        /// <param name="resolution">The resolution</param>
+        /// <param name="securityType">The security type</param>
+        /// <param name="startTimeUtc">The starting date/time (UTC)</param>
+        /// <param name="endTimeUtc">The ending date/time (UTC)</param>
+        public void DownloadAndSave(List<Symbol> symbols, Resolution resolution, SecurityType securityType, TickType tickType, DateTime startTimeUtc, DateTime endTimeUtc)
+        {
+            var writer = new LeanDataWriter(Globals.DataFolder, resolution, securityType, tickType);
+            writer.DownloadAndSave(_brokerage, symbols, startTimeUtc, endTimeUtc);
+        }
+    }
+}
---
+++ QuantConnect.BloombergTests/BloombergDataDownloaderTests.cs
@@ -0,0 +1,45 @@
+﻿/*
+* QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+* Lean Algorithmic Trading Engine v2.2 Copyright 2015 QuantConnect Corporation.
+*/
+
+using System;
+using System.Linq;
+using NUnit.Framework;
+using QuantConnect.Bloomberg;
+using QuantConnect.Configuration;
+using QuantConnect.Logging;
+
+namespace QuantConnect.BloombergTests
+{
+    [TestFixture, Ignore("These tests require a local Bloomberg terminal.")]
+    public class BloombergDataDownloaderTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Log.LogHandler = new ConsoleLogHandler();
+            Config.SetConfigurationFile("../../integration-config.json");
+            Config.Reset();
+        }
+
+        [TestCase("ES", Resolution.Minute)]
+        public void DownloadsFuturesData(string ticker, Resolution resolution)
+        {
+            const SecurityType securityType = SecurityType.Future;
+
+            using (var brokerage = BloombergCommon.CreateBrokerage())
+            {
+                var downloader = new BloombergDataDownloader(brokerage);
+
+                var symbols = downloader.GetChainSymbols(ticker, securityType, true).ToList();
+
+                var startDate = DateTime.UtcNow.Date.AddDays(-15);
+                var endDate = DateTime.UtcNow.Date;
+
+                downloader.DownloadAndSave(symbols, resolution, securityType, TickType.Trade, startDate, endDate);
+                downloader.DownloadAndSave(symbols, resolution, securityType, TickType.Quote, startDate, endDate);
+            }
+        }
+    }
+}
